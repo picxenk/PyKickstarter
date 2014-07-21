9af5aae0c0089a699330e467d4fa3e7ad0c7c49f
SeungBum Kim <picxenk@gmail.com>
2014-07-22 01:11:12 +0900

    add comments

--- ex01.py
+++ ex01.py
@@ -4,14 +4,18 @@
 from PyKickstarter import PyKickstarter
 kick = PyKickstarter()
 
+# search project by keyword
 projects = []
 results = kick.search_projects('microview')
 for p in results.next():
     projects.append(p)
 
+# get the first project data from results
 first = projects[0].data
-# print first
+
+# print attributes in project
 # check the project_block.txt in docs
+# print first
 print first.name
 print first.location['displayable_name']
 print first.backers_count
